0d9a7fe75af92bf0bf9bf56fa49fb2d9429f1c0c
yadavmk0001-sys <yadavmk0001@gmail.com>
2026-06-11 21:54:04 +0530

    Add files via upload

---
+++ Prog3.py
@@ -0,0 +1,6 @@
+country_code = {'India':'0091', 'Australia':'0025', 'Nepal':'00977'}
+
+print("Country code for India : ")
+print(country_code.get('India', 'Not found'))
+print("Country code of Japan : ")
+print(country_code.get('Japan', 'Not found'))
